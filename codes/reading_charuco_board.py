e05f524b472ad86107154b33a1d7685643a9a583
Enes Ağırman <99555923+EnesAgirman@users.noreply.github.com>
2023-07-06 14:14:28 +0200

    Changed back the code that doesn't work at all

--- codes/reading_charuco_board.py
+++ codes/reading_charuco_board.py
@@ -10,88 +10,14 @@ def findArucoMarkers(aImage, aDict):
     print(myID)
     
     aruco.drawDetectedMarkers(aImage, boundaryBoxes, myID)
-    return (boundaryBoxes, myID, rejectedImages)
-
-def readCharucoBoard(aDict, aImage, aBoard):
-    myImageGrayScaled = aImage
-    markerCorners, markerIds, rejected = cv.aruco.detectMarkers(myImageGrayScaled, aDict)
-    myCharucoCorners, myCharucoIds,  = cv.aruco.interpolateCornersCharuco(markerCorners, markerIds, myImageGrayScaled, aBoard)
-    cv.aruco.drawDetectedCornersCharuco(aImage, myCharucoCorners, myCharucoIds)
-
-
 
 def playVideo():
     myVid = cv.VideoCapture(0)
-    myDict = aruco.Dictionary_get(aruco.DICT_7X7_100)
-    
-    myGridBoard = cv.aruco.CharucoBoard.create(7, 5, 0.04, 0.025, myDict)
-    
-    # ChAruco board variables
-    CHARUCOBOARD_ROWCOUNT = 7
-    CHARUCOBOARD_COLCOUNT = 5 
-    ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_5X5_1000)
-
-# Create constants to be passed into OpenCV and Aruco methods
-    CHARUCO_BOARD = aruco.CharucoBoard_create(
-        squaresX=CHARUCOBOARD_COLCOUNT,
-        squaresY=CHARUCOBOARD_ROWCOUNT,
-        squareLength=0.04,
-        markerLength=0.02,
-        dictionary=ARUCO_DICT)
-    
-    corners_all = [] # Corners discovered in all images processed
-    ids_all = [] # Aruco ids corresponding to corners discovered
-    image_size = None # Determined at runtime
 
     while 0xFF != ord(' '):
         captureSuccessfull, myFrame = myVid.read()
         
-        gray = cv.cvtColor(myFrame, cv.COLOR_BGR2GRAY)
-        
-        corners, ids, _ = aruco.detectMarkers(image=gray,dictionary=myDict)
-        
-        img = aruco.drawDetectedMarkers(image=img, corners=corners)
-        
-        response, charuco_corners, charuco_ids = aruco.interpolateCornersCharuco(
-            markerCorners=corners,
-            markerIds=ids,
-            image=gray,
-            board=CHARUCO_BOARD)
-        
-        if response > 20:
-            # Add these corners and ids to our calibration arrays
-            corners_all.append(charuco_corners)
-            ids_all.append(charuco_ids)
-            
-            # Draw the Charuco board we've detected to show our calibrator the board was properly detected
-            img = aruco.drawDetectedCornersCharuco(
-                image=img,
-                charucoCorners=charuco_corners,
-                charucoIds=charuco_ids)
-       
-        # If our image size is unknown, set it now
-        if not image_size:
-            image_size = gray.shape[::-1]
-    
-            # Reproportion the image, maxing width or height at 1000
-            proportion = max(img.shape) / 1000.0
-            img = cv.resize(img, (int(img.shape[1]/proportion), int(img.shape[0]/proportion)))
-            # Pause to display each image, waiting for key press
-            cv.imshow('Charuco board', img)
-            cv.waitKey(0)
-        else:
-            print("Not able to detect a charuco board in image")
-
-        
-        
-        
-        
-        
-        
-        
-        
-        # boundaryBoxes, myID, rejectedImages = findArucoMarkers(myFrame, myDict )
-        readCharucoBoard( myDict, myFrame, myGridBoard )
+        findArucoMarkers(myFrame, aruco.Dictionary_get(aruco.DICT_7X7_100) )
         cv.imshow("My Video", myFrame)
         cv.waitKey(50)
         
