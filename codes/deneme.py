8a22e4b26721c109518018b6cea9f45863180617
Enes Ağırman <99555923+EnesAgirman@users.noreply.github.com>
2023-07-11 15:22:26 +0200

    now i can get the camera Matrix, distortion coefficients, rvecs and tvecs from the charuco board image and draw the coordinate axis but the z-axis is drawn incorrectly

--- codes/deneme.py
+++ codes/deneme.py
@@ -6,11 +6,11 @@ import time
 import pickle
 import glob
 
-def readCharuco(aImage, aDict, aCharucoBoard):
+def readCharuco(aImage, aDict, aCharucoBoard, aCount):
     
     corners_all = [] # Corners discovered in all images processed
     ids_all = [] # Aruco ids corresponding to corners discovered
-    image_size = None # Determined at runtime
+    image_size = (720, 1280)
 
     myImageGrayScale = cv.cvtColor(aImage, cv.COLOR_BGR2GRAY)
     myCorners, myIDs, _ = aruco.detectMarkers( image=myImageGrayScale, dictionary=aDict)
@@ -18,42 +18,68 @@ def readCharuco(aImage, aDict, aCharucoBoard):
     aImage = aruco.drawDetectedMarkers(image=aImage, corners=myCorners) # add ids=myIDs to also sraw id's of aruco markers
     
     if myIDs is None:
+        # if myIDs doesn't exist, it does this
         print("\nhehehhehee boooi\n")
     else:
-        # if myIDs doesn't exist, it does this
+        # if myIDs exist, it does this
         response, charuco_corners, charuco_ids = aruco.interpolateCornersCharuco(markerCorners=myCorners,markerIds=myIDs,image=myImageGrayScale,board=aCharucoBoard,cameraMatrix=None, distCoeffs=None, minMarkers=1)
         
-        corners_all.append(myCorners)
-        ids_all.append(myIDs)
+        corners_all.append(charuco_corners)
+        ids_all.append(charuco_ids)
+        
+        # corners_all = np.squeeze(corners_all)
+        # myIDs = np.squeeze(myIDs)
         
         
         # Draw the Charuco board we've detected to show our calibrator the board was properly detected
         aImage = aruco.drawDetectedCornersCharuco(
                 image=aImage,
                 charucoCorners=charuco_corners, charucoIds=charuco_ids ) # charucoIds=charuco_ids for displaying ids 
+        
+        # Resizing the image
+        # mySize = (aImage.shape[1]//2, aImage.shape[0]//2)
+        # aImage = cv.resize(aImage, mySize, interpolation=cv.INTER_CUBIC)
+        
+        
+        calibration, cameraMatrix, distCoeffs, rvecs, tvecs = aruco.calibrateCameraCharuco(charucoCorners=corners_all,
+                                                                                           charucoIds=ids_all,
+                                                                                           board=aCharucoBoard,
+                                                                                           imageSize=image_size,
+                                                                                           cameraMatrix=None,
+                                                                                           distCoeffs=None)
+        
+        print(rvecs)
+        cv.drawFrameAxes(image=aImage, cameraMatrix=cameraMatrix, distCoeffs=distCoeffs, rvec=rvecs[0], tvec=tvecs[0], length=0.3, thickness=3)
+        
+        print(rvecs)
+        
+        return aImage
+        
+        
+        
+        
+        
 
    
-def playVideo(aDict, aCharucoBoard):
+def readFile(aDict, aCharucoBoard):
     myVid = cv.VideoCapture(0)
     
-    a = -1
+    count = -1
 
     for myFileName in glob.glob('images\webcam_calibration/*.jpg'):
-        a = a+1
+        count = count+1
         myFrame = cv.imread(myFileName)
-        cv.imshow(myFileName, cv.imread(myFileName))
+        
+        cv.imshow(myFileName, myFrame)
 
-        readCharuco(myFrame, aDict, aCharucoBoard)
+        myFrame = readCharuco(myFrame, aDict, aCharucoBoard, count)
 
-
-
-        cv.imshow("My Video" + str(a), myFrame)
-        cv.waitKey(50)
+        cv.imshow("My Video" + str(count), myFrame)
+        
+        cv.waitKey(0)
         
         if cv.waitKey(1) & 0xFF == ord(' '):
             break
-
-myDict = aruco.Dictionary_get(aruco.DICT_7X7_100 )
 
 xNum = 7    # The number of squares in the X direction
 yNum = 5    # The number of squares in the Y direction
@@ -61,14 +87,15 @@ yNum = 5    # The number of squares in the Y direction
 squareSize = 0.02836 # The size of the squares. It is the length of one side
 markerSize = 0.01772 # The size of the aruco markers. It is the length of one side
 
-myDict = aruco.Dictionary_get(aruco.DICT_7X7_100 ) # The dictionary that we use for the aruco markers
+myDict = aruco.Dictionary_get(aruco.DICT_7X7_100 ) # The dictionary that we use for the aruco markers my images
+# myDict = aruco.Dictionary_get(aruco.DICT_6X6_1000 ) # The dictionary that we use for the aruco markers for testCase image
 
 imageSize = (7*120, 5*120)  # The size of the image of the charuco board
 
 # The gridboard that includes the charuco board information
 myGridBoard = cv.aruco.CharucoBoard.create(xNum, yNum, squareSize, markerSize, myDict)
 
-playVideo(myDict, myGridBoard)
+readFile(myDict, myGridBoard)
 
 cv.waitKey(0)
 
